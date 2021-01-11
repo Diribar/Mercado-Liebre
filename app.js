60cb849114beed913fb96ab5d12c88cd5f646240
Diribar <diegoiribarren2015@gmai.com>
2021-01-11 07:39:26 -0300

    clase 11 revisada

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 let path = require('path')
 
-app.listen(3030, () => console.log('Vamos bien'))
+app.listen(3030, () => console.log('Servidor funcionando'))
 app.use(express.static(path.resolve('public')));
 
 app.get('/', (req, res) => {
