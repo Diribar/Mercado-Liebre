ba32304bbe1e9b23d3b32b62113cfebdbdb31083
Diribar <diegoiribarren2015@gmail.com>
2021-01-27 17:40:24 -0300

    entrega del 22-ene-21

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ let path = require('path')
 app.listen(3000, () => console.log('Servidor funcionando...'))
 app.use(express.static(path.resolve('public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.html'))
-})
+/* HOME */      app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))})
+/* REGISTER */  app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home-register.html'))})
+/* LOGIN */  app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home-login.html'))})
