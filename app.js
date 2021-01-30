c3d27498ca2ba2113d5a0541fb2cbe3940a3c6ea
Diribar <diegoiribarren2015@gmail.com>
2021-01-30 00:07:05 -0300

    Agregado de Procfile

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 let path = require('path')
 
-app.listen(3000, () => console.log('Servidor funcionando...'))
+/* app.listen(3000, () => console.log('Servidor funcionando...')) */
+app.listen(process.env.PORT || 3000, () => console.log('Servidor funcionando en el puerto 3000...'))
 app.use(express.static(path.resolve('public')));
 
 /* HOME */      app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))})
