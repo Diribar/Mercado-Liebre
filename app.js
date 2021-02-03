3d7e9fd70100049d43f7e362ec876fd50b142b55
Diribar <diegoiribarren2015@gmail.com>
2021-02-02 21:58:51 -0300

    procfile

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ let path = require('path')
 app.listen(process.env.PORT || 3000, () => console.log('Servidor funcionando en el puerto 3000...'))
 app.use(express.static(path.resolve('public')));
 
-/* HOME */      app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home.html'))})
-/* REGISTER */  app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home-register.html'))})
-/* LOGIN */  app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, 'views/home-login.html'))})
+/* HOME */      app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname, './views/home.html'))})
+/* REGISTER */  app.get('/register', (req, res) => {res.sendFile(path.resolve(__dirname, './views/home-register.html'))})
+/* LOGIN */  app.get('/login', (req, res) => {res.sendFile(path.resolve(__dirname, './views/home-login.html'))})
